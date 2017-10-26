0d34c7f9a7baf513872fd3b747459532dec5e833
Sam Cross <SamCross@Sams-MBP-5.flashtalking.lds>
2017-10-26 11:28:54 +0100

    add changeScore on counter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,62 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 class Counter extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      value: 0
+    }
+    // this.handleIncrement = this.handleIncrement.bind(this);
+    // this.handleDecrement = this.handleDecrement.bind(this);
+    this.scoreChange = this.scoreChange.bind(this);
+  }
+  // handleIncrement () {
+  //   this.setState({
+  //     value: this.state.value + 1
+  //   });
+  // }
+  // handleDecrement () {
+
+  //   if (this.state.value === 0){
+  //     return;
+  //   }
+  //   this.setState({
+  //     value: this.state.value - 1
+  //   });
+  //}
+
+  scoreChange (event) {
+    if (event.target.innerHTML === '+'){
+      this.setState({
+        value: this.state.value + 1
+      });
+    }
+    else {
+      if (this.state.value === 0){
+        return;
+      }
+      this.setState({
+        value: this.state.value - 1
+      });
+    }
+  }
   render () {
     return (
       <div className="counter">
-        {this.props.text}
-        {": "}
-        <span className="counter-value">0</span>
-        <button>+</button>
-        <button>-</button>
+        {this.props.header}
+        <span className="counter-value">{this.state.value}</span>
+        <button onClick={this.scoreChange}>+</button>
+        <button onClick={this.scoreChange}>-</button>
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <Counter text='COUNTER'/>,
+  <Counter header='Counter: '/>,
   document.getElementById('root')
 );
 
